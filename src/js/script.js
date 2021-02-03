8937fe28edd27f9928b9f2d8a9c807c4f9900621
Sergey Zimonov <zimonov87@bk.ru>
2021-02-03 23:49:41 +0600

    Some parts added

--- src/js/script.js
+++ src/js/script.js
@@ -48,7 +48,16 @@ let menu = document.querySelector('.header__menu');
 hamburger.addEventListener('click', () => {
    hamburger.classList.toggle('active');
    menu.classList.toggle('active');
+   scroll_stop();
 });
+
+function scroll_stop() {
+   if (document.body.style.overflow !== 'hidden') {
+      document.body.style.overflow = 'hidden';
+   } else {
+      document.body.style.overflow = 'auto';
+   }
+}
 
 // ADAPTIVE HEADER
 
